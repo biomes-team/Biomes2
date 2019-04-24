c9e167fc351467850eccce4dddbb9a767ed2558b
Kevin Fisher <kfish610@gmail.com>
2019-04-24 14:47:02 -0700

    Initial setup and added crystals

---
+++ Source/Biomes2/Biomes2/CompProperties_OreBiomePreferred.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Verse;
+using RimWorld;
+
+namespace Biomes2
+{
+    public class CompProperties_OreBiomePreferred : CompProperties
+    {
+        public List<BiomeDef> allowedBiomes = new List<BiomeDef>();
+        public CompProperties_OreBiomePreferred()
+        {
+            compClass = typeof(CompOreBiomePreferred);
+        }
+    }
+}
