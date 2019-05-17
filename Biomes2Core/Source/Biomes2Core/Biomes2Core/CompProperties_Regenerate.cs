cfaa2a07de2d638aab361e4c13345a2506088b35
Kevin Fisher <kfish610@gmail.com>
2019-05-17 09:41:09 -0700

    Add comp regenerate

---
+++ Biomes2Core/Source/Biomes2Core/Biomes2Core/CompProperties_Regenerate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace Biomes2Core
+{
+    public class CompProperties_Regenerate : CompProperties
+    {
+        public float amount = 0.1f;
+        public float startThreshold = 0.2f;
+        public float endThreshold = 1f;
+        public bool restoreBodyParts = false;
+        public int delay = 15;
+        public CompProperties_Regenerate()
+        {
+            compClass = typeof(CompRegenerate);
+        }
+    }
+}
---
+++ Biomes2Core/Source/Biomes2Core/Biomes2Core/CompRegenerate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace Biomes2Core
+{
+    public class CompRegenerate : ThingComp
+    {
+        bool restoring = false;
+        int lastheal = 0;
+        public CompProperties_Regenerate Props
+        {
+            get
+            {
+                return (CompProperties_Regenerate)props;
+            }
+        }
+
+        public override void CompTick()
+        {
+            Pawn pawn = (Pawn)parent;
+            if (((Pawn)parent).health.hediffSet.GetPartHealth(((Pawn)parent).RaceProps.body.corePart) < ((Pawn)parent).RaceProps.body.corePart.def.hitPoints * Props.startThreshold * ((Pawn)parent).def.race.baseHealthScale)
+            {
+                if (GenTicks.TicksGame > lastheal + GenTicks.SecondsToTicks(Props.delay))
+                {
+                    foreach (Hediff h in pawn.health.hediffSet.hediffs)
+                    {
+                        if (h.def == HediffDefOf.Cut || h.def == HediffDefOf.Stab || h.def == HediffDefOf.Bruise || h.def == HediffDefOf.Bite || h.def == HediffDefOf.Gunshot || h.def == HediffDefOf.Scratch || h.def == HediffDefOf.Shredded || h.def == HediffDef.Named("Crush") || h.def == HediffDef.Named("Crack") || (h.def == HediffDefOf.MissingBodyPart && Props.restoreBodyParts))
+                        {
+                            h.Heal(Props.amount);
+                        }
+                    }
+                    lastheal = GenTicks.TicksGame;
+                }
+                restoring = true;
+            }
+            if (restoring && ((Pawn)parent).health.hediffSet.GetPartHealth(((Pawn)parent).RaceProps.body.corePart) >= ((Pawn)parent).RaceProps.body.corePart.def.hitPoints * Props.endThreshold * ((Pawn)parent).def.race.baseHealthScale)
+            {
+                restoring = false;
+            }
+            base.CompTick();
+        }
+    }
+}
